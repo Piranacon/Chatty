02a4cdc484ac127a72d4bd01a100ec34adc11f11
Parky (Mac) <park.paul@outlook.com>
2023-04-29 14:02:06 +0100

    Added the basics to the project

---
+++ Chatty.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Chatty.UI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Chatty.UI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Chatty.UI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
